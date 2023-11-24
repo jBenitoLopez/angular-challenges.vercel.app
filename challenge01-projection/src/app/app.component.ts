d0d867bee97e622013716204051999345c98abe6
jBenitoLopez <lopez.jbenito@gmail.com>
2023-11-24 19:53:26 +0100

    challenge01-projection 01

--- challenge01-projection/src/app/app.component.ts
+++ challenge01-projection/src/app/app.component.ts
@@ -2,13 +2,21 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { CityCardComponent } from './component/city-card/city-card.component';
+import { StudentCardComponent } from './component/student-card/student-card.component';
+import { TeacherCardComponent } from './component/teacher-card/teacher-card.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    TeacherCardComponent,
+    StudentCardComponent,
+    CityCardComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'challenge01-projection';
-}
+export class AppComponent {}
