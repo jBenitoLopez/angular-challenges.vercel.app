7f72f17e221e789b9a3d7e9a6e4be9c8c4816374
jblopez <lopez.jbenito@gmail.com>
2023-11-25 13:37:22 +0100

    adapt student and techer cart to new card comp

--- challenge01-projection/src/app/component/student-card/student-card.component.ts
+++ challenge01-projection/src/app/component/student-card/student-card.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FakeHttpService } from '../../data-access/fake-http.service';
+import { FakeHttpService, randStudent } from '../../data-access/fake-http.service';
 import { StudentStore } from '../../data-access/student.store';
-import { CardType } from '../../model/card.model';
+// import { CardType } from '../../model/card.model';
 import { Student } from '../../model/student.model';
 import { CardComponent } from '../../ui/card/card.component';
 
@@ -9,27 +9,27 @@ import { CardComponent } from '../../ui/card/card.component';
   selector: 'app-student-card',
   template: `<app-card
     [list]="students"
-    [type]="cardType"
-    customClass="bg-light-green"></app-card>`,
+    customClass="bg-light-green"
+    (clickNewItem)="addNewItem()"
+    >
+      <img src="assets/img/student.webp" width="200px" />
+    </app-card>`,
   standalone: true,
-  styles: [
-    `
-      ::ng-deep .bg-light-green {
-        background-color: rgba(0, 250, 0, 0.1);
-      }
-    `,
-  ],
+
   imports: [CardComponent],
 })
 export class StudentCardComponent implements OnInit {
   students: Student[] = [];
-  cardType = CardType.STUDENT;
 
-  constructor(private http: FakeHttpService, private store: StudentStore) {}
+  constructor(private http: FakeHttpService, private store: StudentStore) { }
 
   ngOnInit(): void {
     this.http.fetchStudents$.subscribe((s) => this.store.addAll(s));
 
     this.store.students$.subscribe((s) => (this.students = s));
+  }
+
+  addNewItem() {
+    this.store.addOne(randStudent());
   }
 }
--- challenge01-projection/src/app/ui/card/card.component.ts
+++ challenge01-projection/src/app/ui/card/card.component.ts
@@ -1,34 +1,44 @@
-import { NgFor, NgIf } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { randStudent, randTeacher } from '../../data-access/fake-http.service';
-import { StudentStore } from '../../data-access/student.store';
-import { TeacherStore } from '../../data-access/teacher.store';
-import { CardType } from '../../model/card.model';
+import { NgFor } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+// import { randStudent, randTeacher } from '../../data-access/fake-http.service';
+// import { StudentStore } from '../../data-access/student.store';
+// import { TeacherStore } from '../../data-access/teacher.store';
+// import { CardType } from '../../model/card.model';
 import { ListItemComponent } from '../list-item/list-item.component';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   standalone: true,
-  imports: [NgIf, NgFor, ListItemComponent],
+  // imports: [NgIf, NgFor, ListItemComponent],
+  imports: [NgFor, ListItemComponent],
+  styles: [
+    `
+      .bg-light-red {
+        background-color: rgba(250, 0, 0, 0.1);
+      }
+
+      .bg-light-green {
+        background-color: rgba(0, 250, 0, 0.1);
+      }
+    `,
+  ],
 })
 export class CardComponent {
   @Input() list: any[] | null = null;
-  @Input() type!: CardType;
   @Input() customClass = '';
+  @Output() clickNewItem: EventEmitter<any> = new EventEmitter<any>();
 
-  CardType = CardType;
+  constructor() { }
 
-  constructor(
-    private teacherStore: TeacherStore,
-    private studentStore: StudentStore
-  ) {}
-
-  addNewItem() {
-    if (this.type === CardType.TEACHER) {
-      this.teacherStore.addOne(randTeacher());
-    } else if (this.type === CardType.STUDENT) {
-      this.studentStore.addOne(randStudent());
-    }
+  add() {
+    console.log('add');
   }
+  // addNewItem() {
+  //   if (this.type === CardType.TEACHER) {
+  //     this.teacherStore.addOne(randTeacher());
+  //   } else if (this.type === CardType.STUDENT) {
+  //     this.studentStore.addOne(randStudent());
+  //   }
+  // }
 }
